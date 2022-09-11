8c62225638d5d2e9f587f605dfeab36e1de6bd2c
FiqihPavita <21083010042@student.upnjatim.ac.id>
2022-09-11 17:30:07 +0700

    Tugas1.pdf

---
+++ Tugas1.sh
@@ -0,0 +1,34 @@
+pwd
+ls
+ls -l
+cd
+cat
+nano
+mkdir
+rm
+rmdir
+rm-rf
+mv
+cp
+df
+top
+free
+python3
+version
+whereis
+which
+whatis
+locate&find
+ping
+python3 
+print("----------------------------------------")
+print("Tugas 1 SISOP Kelas-A")
+print("----------------------------------------")
+print("Nama: Fiqih Pavita Andharluana")
+print("NPM: 21083010042")
+print("----------------------------------------")
+print("Notes:")
+print("----------------------------------------")
+print("Hallo dunia!")
+print("Ini adalah skrip Bash pertamaku di Linux")
+
